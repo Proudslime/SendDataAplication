4aecffc1552e5e1d965adefdf0b266ecfe3b9007
Proudslime <36756353+Proudslime@users.noreply.github.com>
2020-09-19 09:39:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/senddataaplication/SendMessageActivity.java
@@ -0,0 +1,41 @@
+package com.example.senddataaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    public String message = "successful";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+
+        Intent intent = getIntent();
+        String txt = intent.getStringExtra("Data");
+
+        textView = findViewById(R.id.showText);
+
+        textView.setText(txt);
+
+        Button button = (Button)findViewById(R.id.btnReturn);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SendMessageActivity.this,MainActivity.class);
+                intent.putExtra("return",message);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
